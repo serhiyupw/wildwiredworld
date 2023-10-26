3e19daca7962f3b8f9dfa95f47558f7c56f74e0c
serhiyupw <serhiyupw@gmail.com>
2023-10-26 01:51:13 +0100

    test

---
+++ components/BackgroundVideo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="background-video">
+    <video autoplay muted loop playsinline class="video">
+      <source src="home.mp4" type="video/mp4">
+      <!-- You can include other video sources for different formats -->
+    </video>
+  </div>
+</template>
+
+
+
+<style scoped>
+.background-video {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1; /* Place it below other content */
+  overflow: hidden; /* Ensure the video remains within the viewport */
+}
+
+.video {
+  object-fit: cover; /* Cover the entire container */
+  width: 100%;
+  height: 100%;
+  filter: brightness(50%);
+}
+</style>
